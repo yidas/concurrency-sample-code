b1d3c24e99d882c9138c2fd0bbc0301c42192c66
Nick Tsai <myintaer@gmail.com>
2024-04-22 16:40:18 +0800

    Create concurrent-futures-ThreadPoolExecutor.py

---
+++ python/concurrent-futures-ThreadPoolExecutor.py
@@ -0,0 +1,25 @@
+import concurrent.futures
+import time, random
+
+def worker(task):
+    # For monitoring I/O
+    time.sleep(random.uniform(0.1, 5))
+    print(task)
+    return task
+
+if __name__ == '__main__':
+    # Define tasks
+    tasks = range(1000)
+
+    worker_num = len(tasks)
+
+    # Initialize process pool executor
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=worker_num) as executor:
+        # Asynchronously process tasks using submit
+        futures = [executor.submit(worker, task) for task in tasks]
+
+        # Get results from the async results
+        final_results = [future.result() for future in concurrent.futures.as_completed(futures)]
+
+    print("Final Results:", final_results)
